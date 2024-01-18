9a7ccb13839cdcffa7cf1ab6c213ef763819c30e
omicron <131537767+omicr0nn@users.noreply.github.com>
2024-01-18 14:45:54 +0300

    Update install_libraries.bat

--- install_libraries.bat
+++ install_libraries.bat
@@ -2,7 +2,7 @@
 echo Installing required Python libraries...
 
 python.exe -m pip install --upgrade pip
-pip install requests
+pip install urllib3 httpx requests
 
 echo Libraries installed successfully.
 pause
